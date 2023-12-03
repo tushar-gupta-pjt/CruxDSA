5f3c01d24b1edb561cf0b466c4ddd31d85089d9b
Tushar Gupta <tggguptatushar@gmail.com>
2023-12-03 15:12:34 +0530

    BST

---
+++ Crux/src/lecture_38/Insert_into_a_Binary_Search_Tree.java
@@ -0,0 +1,39 @@
+package lecture_38;
+// leetcode 701
+public class Insert_into_a_Binary_Search_Tree {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
+	class Solution {
+	    public TreeNode insertIntoBST(TreeNode root, int val) {
+	    	if(root == null) {
+	    		return new TreeNode(val);                                               // this nodes address will go back to where null was called from!!!
+	    	}
+	    	if(root.val < val) {
+	    		root.right = insertIntoBST(root.right, val);
+	    	}
+	    	else {
+	    		root.left = insertIntoBST(root.left, val);
+	    	}
+	    	return root;
+	        
+	    }
+	}
+
+}
---
+++ Crux/src/lecture_38/Validate_Binary_Search_Tree.java
@@ -0,0 +1,56 @@
+package lecture_38;
+
+
+
+public class Validate_Binary_Search_Tree {
+	
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
+	class Solution {
+	    public boolean isValidBST(TreeNode root) {
+	    	
+	    	BstPair ans = ValidBST(root);
+	    	
+	    	return ans.isbst;
+	        
+	    }
+	    
+	    public BstPair ValidBST(TreeNode root) {
+	    	if(root == null) {
+	    		return new BstPair();
+	    	}	
+	    		BstPair lbstp = ValidBST(root.left);
+	    		BstPair rbstp = ValidBST(root.right);
+	    		BstPair sbstp = new BstPair();
+	    		sbstp.min = Math.min(root.val, Math.min(lbstp.min, rbstp.min));
+	    		sbstp.max = Math.max(root.val, Math.max(lbstp.max, rbstp.max));
+	    		sbstp.isbst = lbstp.isbst && rbstp.isbst && lbstp.max<root.val && rbstp.min>root.val;
+	    		return sbstp;
+	    	}
+	    class BstPair{
+	    	boolean isbst = true;
+	    	Long min = Long.MAX_VALUE;
+	    	Long max = Long.MIN_VALUE;
+	    }
+	}
+	
+	
+
+}
